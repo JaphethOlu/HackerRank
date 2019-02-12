5973ded95577239a79679e5b3e6545f10242ab43
Ishola Japheth <japhethish@gmail.com>
2019-02-12 19:14:39 +0100

    Updated Folder Names

---
+++ 30 Days HackerRank/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] inputArray = Convert.ToString(Console.ReadLine());
+            int outputArraySize = inputArray.Length + 1;
+
+            string[] outputArray = new String[outputArraySize];
+
+            int outputArrayPosition = 0;
+            for(int i = inputArray.Length + 1; i >= 0; i--)
+            {
+                outputArray[outputArrayPosition] = inputArray[i];   
+                outputArrayPosition++;
+            }
+
+            WriteLine(outputArray.ToString());
+        }
+    }
+}
