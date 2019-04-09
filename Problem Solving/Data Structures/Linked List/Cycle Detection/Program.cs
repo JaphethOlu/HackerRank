e77d7d06cc6d3121449ac63062f9116a77cca6c1
Ishola Japheth <japhethish@gmail.com>
2019-04-09 16:28:22 +0200

    More LL

---
+++ Problem Solving/Data Structures/Linked List/Cycle Detection/Program.cs
@@ -0,0 +1,160 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter) {
+        while (node != null) {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static bool hasCycle(SinglyLinkedListNode head) {
+        
+        /*
+        // Only Test Case 0 and 2 because of Time Complexity
+        if(head == null || head.next == null)
+        {
+            return false;
+        }
+        else
+        {
+            var currentNode = head.next;
+            while(currentNode != null)
+            {
+                if(head == currentNode)
+                {
+                    return true;
+                }
+                currentNode = currentNode.next;
+            }
+            return hasCycle(head.next);
+        }
+        */
+
+        // All Test Cases pass because of Time and Space Complexity Trade-Off
+        var nodes = new HashSet<SinglyLinkedListNode>();
+        if(head == null || head.next == null)
+        {
+            return false;
+        }
+        else
+        {
+            while(head != null)
+            {
+                if(nodes.Contains(head))
+                {
+                    return true;
+                }
+                else
+                {
+                    nodes.Add(head);
+                    head = head.next;
+                }
+            }
+            return false;
+        }
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+          
+          	SinglyLinkedListNode extra = new SinglyLinkedListNode(-1);
+            SinglyLinkedListNode temp = llist.head;
+
+            for (int i = 0; i < llistCount; i++) {
+                if (i == index) {
+                    extra = temp;
+                }
+
+                if (i != llistCount-1) {
+                    temp = temp.next;
+                }
+            }
+      
+      		temp.next = extra;
+
+            bool result = hasCycle(llist.head);
+
+            textWriter.WriteLine((result ? 1 : 0));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
